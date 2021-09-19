d33e2c2a35ab1ba0882d5fe10c6f43196d145d9d
Kikkirej <5462557+Kikkirej@users.noreply.github.com>
2021-09-19 21:02:33 +0200

    :bug: id added to cloud creation.

--- src/main/java/net/fabcelhaft/msteamsapprovalexample/dto/CloudCreationDTO.java
+++ src/main/java/net/fabcelhaft/msteamsapprovalexample/dto/CloudCreationDTO.java
@@ -1,6 +1,7 @@
 package net.fabcelhaft.msteamsapprovalexample.dto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import net.fabcelhaft.msteamsapprovalexample.controller.SettingsController;
@@ -9,14 +10,18 @@ import net.fabcelhaft.msteamsapprovalexample.model.Approvement;
 @Data
 @NoArgsConstructor
 @ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 public class CloudCreationDTO extends CreationDTO{
     public CloudCreationDTO(Approvement approvment){
         this.setApprovers(approvment.getPotentialApprovers());
         this.setName(approvment.getName());
         this.setDescription(approvment.getDescription());
         this.setAllApproversNeeded(approvment.getAllApproversNeeded());
+        this.id=approvment.getId();
         this.url= SettingsController.baseUrl+"/approvements/" + approvment.getId();
     }
+
+    private Long id;
 
     private String url;
 }
